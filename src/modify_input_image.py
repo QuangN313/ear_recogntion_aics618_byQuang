2586867d2422768592932b4c2dea53f6421f51eb
QuangN313 <nh.quang313@gmail.com>
2018-10-25 16:07:12 +0700

    rename_input_image

---
+++ src/modify_input_image.py
@@ -0,0 +1,17 @@
+import os
+import os.path
+from os import listdir
+from os.path import isfile, join
+def rename():
+    pathAWE = os.path.join(DATA_DIR,"awe")
+    pathFiles = [ os.path.join(pathAWE,f) for f in listdir(pathAWE) if isfile(join(pathAWE,f)) == 0]
+    for fs in pathFiles:
+        for re_f in listdir(fs):
+            namefile = os.path.basename(fs)
+            if ".png" in re_f:
+                os.rename(re_f,namefile + "_" + re_f )
+            
+if __name__ == "__main__" :
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DATA_DIR = os.path.join(BASE_DIR,'data')
+    rename()
