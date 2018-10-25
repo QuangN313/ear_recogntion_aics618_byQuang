a86b92999bc7fabcfbe3441e8a9b3e857cf3f4df
QuangN313 <nh.quang313@gmail.com>
2018-10-25 16:36:05 +0700

    rename_input_image_folername_fileName_png

--- src/modify_input_image.py
+++ src/modify_input_image.py
@@ -9,8 +9,8 @@ def rename():
         for re_f in listdir(fs):
             namefile = os.path.basename(fs)
             if ".png" in re_f:
-                os.rename(re_f,namefile + "_" + re_f )
-            
+                os.rename(os.path.join(fs,re_f),os.path.join(fs,namefile + "_" + re_f ))
+                # print("1")
 if __name__ == "__main__" :
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     DATA_DIR = os.path.join(BASE_DIR,'data')
